fd70f1e9137b85398b8b8cfc101321524037eaf1
Luis Simas_PAROS <gabriel.simas@thera.com.br>
2020-01-01 20:18:10 -0300

    TDD - RED Factor nos testes para Queries de Produtos.

---
+++ Store.Tests/Queries/ProductQueriesTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Store.Tests.Queries
+{
+    [TestClass]
+    public class ProductQueriesTests
+    {
+        [TestMethod]
+        [TestCategory("Queries")]
+        public void DadoAConsultaDeProdutosAtivosDeveRetornarTres()
+        {            
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [TestCategory("Queries")]
+        public void DadoAConsultaDeProdutosInativosDeveRetornarDois()
+        {            
+            Assert.Fail();
+        }
+    }
+}
