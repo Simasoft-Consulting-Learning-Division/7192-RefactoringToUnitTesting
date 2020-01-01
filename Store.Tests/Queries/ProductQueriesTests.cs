4ee163fca5d157a23d3e4ff74d94ce9f17da3983
Luis Simas_PAROS <gabriel.simas@thera.com.br>
2020-01-01 20:23:33 -0300

    Aplicando TDD - GREEN factor no teste de unidade da Query para Products.

--- Store.Tests/Queries/ProductQueriesTests.cs
+++ Store.Tests/Queries/ProductQueriesTests.cs
@@ -1,22 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Store.Domain.Entities;
 
 namespace Store.Tests.Queries
 {
     [TestClass]
     public class ProductQueriesTests
     {
+        private IList<Product> _products;
+
+        public ProductQueriesTests()
+        {
+            _products = new List<Product>();            
+            _products.Add(new Product("Produto 01", 10, true));
+            _products.Add(new Product("Produto 02", 10, true));
+            _products.Add(new Product("Produto 03", 10, true));
+            _products.Add(new Product("Produto 04", 10, false));
+            _products.Add(new Product("Produto 05", 10, false));
+        }
+        
         [TestMethod]
         [TestCategory("Queries")]
         public void DadoAConsultaDeProdutosAtivosDeveRetornarTres()
-        {            
-            Assert.Fail();
+        {
+            var result = _products.AsQueryable().Where(p => p.Active).Count();                        
+            Assert.AreEqual(result,3);
         }
 
         [TestMethod]
         [TestCategory("Queries")]
         public void DadoAConsultaDeProdutosInativosDeveRetornarDois()
         {            
-            Assert.Fail();
+            var result = _products.AsQueryable().Where(p => !p.Active).Count();                        
+            Assert.AreEqual(result,2);
         }
     }
 }
