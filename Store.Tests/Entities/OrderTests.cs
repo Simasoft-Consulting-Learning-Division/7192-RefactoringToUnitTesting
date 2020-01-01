96e573b0a8ad2cd0aecb3fe4fc7a7312c3a51619
Luis Simas_PAROS <gabriel.simas@thera.com.br>
2020-01-01 18:46:34 -0300

    TDD - Gerando GREEN no Teste DadoUmNovoPedidoValidoDeveGerarUmNumeroComOitoCaracteres

--- Store.Tests/Entities/OrderTests.cs
+++ Store.Tests/Entities/OrderTests.cs
@@ -1,4 +1,6 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Store.Domain.Entities;
 
 namespace Store.Tests.Entities
 {
@@ -9,7 +11,11 @@ namespace Store.Tests.Entities
         [TestCategory("Domain")]
         public void DadoUmNovoPedidoValidoDeveGerarUmNumeroComOitoCaracteres()
         {
-            Assert.Fail();
+            Customer customer = new Customer("Luis Gabriel N. Simas", "gabrielsimas@gmail.com");
+            Discount discount = new Discount(5m,DateTime.Now.AddDays(5));
+            Order order = new Order(customer,0.05m,discount);
+
+            Assert.AreEqual(8, order.Number.Length);
         }
 
         public void DadoUmNovoPedidoSeuStatusDeveSerAguardandoPagamento()
@@ -24,63 +30,63 @@ namespace Store.Tests.Entities
 
         [TestMethod]
         [TestCategory("Domain")]
-        public void Dado_um_pedido_cancelado_seu_status_deve_ser_cancelado()
+        public void DadoUmPedidoCanceladoSeuStatusDeveSerCancelado()
         {
             Assert.Fail();
         }
 
         [TestMethod]
         [TestCategory("Domain")]
-        public void Dado_um_novo_item_sem_produto_o_mesmo_nao_deve_ser_adicionado()
+        public void DadoUmNovoItemSemProdutoOMesmoNaoDeveSerAdicionado()
         {
             Assert.Fail();
         }
 
         [TestMethod]
         [TestCategory("Domain")]
-        public void Dado_um_novo_item_com_quantidade_zero_ou_menor_o_mesmo_nao_deve_ser_adicionado()
+        public void DadoUmNovoItemComQuantidadeZeroOuMenorOMesmoNaoDeveSerAdicionado()
         {
             Assert.Fail();
         }
 
         [TestMethod]
         [TestCategory("Domain")]
-        public void Dado_um_novo_pedido_valido_seu_total_deve_ser_50()
+        public void DadoUmNovoPedidoValidoSeuTotalDeveSerCinquenta()
         {
             Assert.Fail();
         }
 
         [TestMethod]
         [TestCategory("Domain")]
-        public void Dado_um_desconto_expirado_o_valor_do_pedido_deve_ser_60()
+        public void DadoUmDescontoExpiradoOValorDoPedidoDeveSerSessenta()
         {
             Assert.Fail();
         }
 
         [TestMethod]
         [TestCategory("Domain")]
-        public void Dado_um_desconto_invalido_o_valor_do_pedido_deve_ser_60()
+        public void DadoUmDescontoInvalidoOValorDoPedidoDeveSerSessenta()
         {
             Assert.Fail();
         }
 
         [TestMethod]
         [TestCategory("Domain")]
-        public void Dado_um_desconto_de_10_o_valor_do_pedido_deve_ser_50()
+        public void DadoUmDescontoDeDezPorcentoOValorDoPedidoDeveSerCinquenta()
         {
             Assert.Fail();
         }
 
         [TestMethod]
         [TestCategory("Domain")]
-        public void Dado_uma_taxa_de_entrega_de_10_o_valor_do_pedido_deve_ser_60()
+        public void DadoUmaTaxaDeEntregaDeDezOValorDoPedidoDeveSerSessenta()
         {
             Assert.Fail();
         }
 
         [TestMethod]
         [TestCategory("Domain")]
-        public void Dado_um_pedido_sem_cliente_o_mesmo_deve_ser_invalido()
+        public void DadoUmPedidoSemClienteOmesmodeveSerInvalido()
         {
             Assert.Fail();
         }
